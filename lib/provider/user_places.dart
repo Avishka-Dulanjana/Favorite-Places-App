ba28cbfec3d1e2f41d6ca762d6c28297e789fcbc
Avishka-Dulanjana <avishkadulanjana98@gmail.com>
2024-01-13 20:57:47 +0530

    Storing the data in (On-device) SQL database and Loading data from the sql database

--- lib/provider/user_places.dart
+++ lib/provider/user_places.dart
@@ -3,11 +3,43 @@ import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 import 'package:favourite_places/models/place.dart';
 
+Future<Database> _getDatabase() async {
+  final dbpath = await sql.getDatabasesPath();
+  return sql.openDatabase(
+    path.join(dbpath, 'places_db'),
+    onCreate: (db, version) => db.execute(
+        'CREATE TABLE user_places(id TEXT PRIMARY_KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)'),
+    version: 1,
+  );
+}
+
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
+
+  void _loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+    final places = data
+        .map(
+          (row) => Place(
+            id: row['id'] as String,
+            title: row['title'] as String,
+            image: File(row['image'] as String),
+            location: PlaceLocation(
+              latitude: row['lat'] as double,
+              longtitude: row['lng'] as double,
+              address: row['address'] as String,
+            ),
+          ),
+        )
+        .toList();
+    state = places;
+  }
 
   void addPlace(String title, File image, PlaceLocation location) async {
     final appDir = await syspaths.getApplicationDocumentsDirectory();
@@ -16,6 +48,17 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
 
     final newPlace =
         Place(title: title, image: copiedImage, location: location);
+
+    final db = await _getDatabase();
+    db.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'lat': newPlace.location.latitude,
+      'lng': newPlace.location.longtitude,
+      'address': newPlace.location.address
+    });
+
     state = [newPlace, ...state];
   }
 }
